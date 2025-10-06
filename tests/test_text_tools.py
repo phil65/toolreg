fa05e311f59b15547bcd58fa74a1abfb19a4b80a
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 03:09:32 +0200

    chore: misc

--- tests/test_text_tools.py
+++ tests/test_text_tools.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pytest
 
-from toolreg.tools import text
+from toolreg.tools import text as texttools
 
 
 GIVEN = """def test(sth, averylongvarname, anotherlongvarname, andanother): pass"""
@@ -18,44 +18,46 @@ def test(
 
 
 def test_removesuffix():
-    assert text.removesuffix("Hello, World!", ", World!") == "Hello"
+    assert texttools.removesuffix("Hello, World!", ", World!") == "Hello"
 
 
 def test_removeprefix():
-    assert text.removeprefix("Hello, World!", "Hello, ") == "World!"
+    assert texttools.removeprefix("Hello, World!", "Hello, ") == "World!"
 
 
 def test_lstrip():
-    assert text.lstrip("   Hello, World!  ") == "Hello, World!  "  # noqa: B005
+    assert texttools.lstrip("   Hello, World!  ") == "Hello, World!  "  # noqa: B005
 
 
 def test_rstrip():
-    assert text.rstrip("   Hello, World!  ") == "   Hello, World!"  # noqa: B005
+    assert texttools.rstrip("   Hello, World!  ") == "   Hello, World!"  # noqa: B005
 
 
 def test_format_code():
-    assert text.format_code(GIVEN, line_length=50) == EXPECTED
-    assert text.format_code("invalid code!", line_length=50) == "invalid code!"
+    assert texttools.format_code(GIVEN, line_length=50) == EXPECTED
+    assert texttools.format_code("invalid code!", line_length=50) == "invalid code!"
 
 
 def test_format_signature():
     def test_function(a, b, c: int = 1, *args, **kwargs):
         pass
 
-    assert text.format_signature(test_function) == "(a, b, c: int = 1, *args, **kwargs)"
     assert (
-        text.format_signature(test_function, eval_str=False)
+        texttools.format_signature(test_function) == "(a, b, c: int = 1, *args, **kwargs)"
+    )
+    assert (
+        texttools.format_signature(test_function, eval_str=False)
         == "(a, b, c: 'int' = 1, *args, **kwargs)"
     )
 
 
 def test_slugify():
-    assert text.slugify("Hello, World!") == "hello__world_"
+    assert texttools.slugify("Hello, World!") == "hello__world_"
 
 
 if __name__ == "__main__":
     code = "def test(sth, fsjkdalfjksdalfjsadk, fjskldjfkdsljf, fsdkjlafjkdsafj): pass"
-    text = text.format_code(code, line_length=50)
+    text = texttools.format_code(code, line_length=50)
     print(text)
 
 
